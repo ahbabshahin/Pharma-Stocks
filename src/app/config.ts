01a2fd7bbc6010b59cb99242c13fe54ea6c8e800
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2025-01-21 23:05:51 +0600

    config file updated

--- src/app/config.ts
+++ src/app/config.ts
@@ -4,6 +4,6 @@ import { Injectable } from "@angular/core";
   providedIn: 'root',
 })
 export class Config {
-  rootURL = 'http://localhost:5000/api';
-  // rootURL = 'https://pharma-stocks-server.vercel.app/api';
+  // rootURL = 'http://localhost:5000/api';
+  rootURL = 'https://pharma-stocks-server.vercel.app/api';
 }
