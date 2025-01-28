58a69ffbf82ec31bffc93e7f0d7b79ea8d219170
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2025-01-28 23:43:26 +0600

    fetch all user added

--- src/app/modules/secure/user/user.component.ts
+++ src/app/modules/secure/user/user.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from '../../../store/models/user.model';
+import { UserApiService } from '../../../service/user/user-api.service';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'app-user',
@@ -10,8 +12,32 @@ export class UserComponent {
   user!: any;
   loader: boolean = true;
   users: User[] = [];
-  ngOnInit(){
+  subs = new SubSink();
+  params = {
+    page: 1,
+    limit: 10
+  }
+  total: number = 0;
+  constructor(private userApi: UserApiService){}
 
+  ngOnInit(){
+    this.initialize();
+  }
+
+  initialize(){
+    this.getUsers();
+  }
+
+  getUsers(){
+    this.subs.sink = this.userApi
+      .getUsers(this.params)
+      .subscribe({ next: (res: any) => {
+        this.users = res.body;
+        this.total = res?.total
+        this.loader = false;
+      }, error: () => {
+        this.loader = false;
+      } });
   }
 
   editRole(user: User){
--- src/app/modules/secure/user/user.module.ts
+++ src/app/modules/secure/user/user.module.ts
@@ -4,15 +4,13 @@ import * as userComponents from './index';
 import { UserRoutingModule } from './user-routing.module';
 import { SharedModule } from '../../../shared/shared.module';
 import { LoaderComponent } from '../../../common-component/loader/loader.component';
+import { UserApiService } from '../../../service/user/user-api.service';
+import { UserStoreService } from '../../../service/user/user-store.service';
 
 
 @NgModule({
   declarations: [...userComponents.components],
-  imports: [
-    CommonModule,
-    UserRoutingModule,
-    SharedModule,
-    LoaderComponent,
-  ],
+  imports: [CommonModule, UserRoutingModule, SharedModule, LoaderComponent],
+  providers: [UserApiService, UserStoreService],
 })
 export class UserModule {}
