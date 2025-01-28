f6c0283f481d47fbd927407eaf05e33e30d6f4d3
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2025-01-28 23:13:01 +0600

    User component updated

--- src/app/modules/secure/user/user.component.ts
+++ src/app/modules/secure/user/user.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { User } from '../../../store/models/user.model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
-  styleUrl: './user.component.scss'
+  styleUrl: './user.component.scss',
 })
 export class UserComponent {
+  user!: any;
+  loader: boolean = true;
+  users: User[] = [];
+  ngOnInit(){
 
+  }
+
+  editRole(user: User){
+
+  }
+
+  ngOnDestroy(){
+
+  }
 }
--- src/app/modules/secure/user/user.module.ts
+++ src/app/modules/secure/user/user.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import * as userComponents from './index';
 import { UserRoutingModule } from './user-routing.module';
+import { SharedModule } from '../../../shared/shared.module';
+import { LoaderComponent } from '../../../common-component/loader/loader.component';
 
 
 @NgModule({
   declarations: [...userComponents.components],
-  imports: [CommonModule, UserRoutingModule],
+  imports: [
+    CommonModule,
+    UserRoutingModule,
+    SharedModule,
+    LoaderComponent,
+  ],
 })
 export class UserModule {}
