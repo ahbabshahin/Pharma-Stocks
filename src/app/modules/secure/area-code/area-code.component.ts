872f5871f7beae3927f38440354698270910897c
Ahbab Shahin Chowdhury <ahbabshahin7788@gmail.com>
2025-12-14 15:12:17 +0600

    added mobile friendly options for drawer, opening full drawer if it's mobile else half drawer

--- src/app/modules/secure/area-code/area-code.component.ts
+++ src/app/modules/secure/area-code/area-code.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { AreaCodeListComponent } from 'src/app/modules/secure/area-code/area-code-list/area-code-list.component';
 import { AreaCodeStoreService } from 'src/app/service/area-code/area-code-store.service';
-import { Observable, of, Subject, takeUntil } from 'rxjs';
+import { lastValueFrom, Observable, of, Subject, takeUntil } from 'rxjs';
 import { LoaderComponent } from 'src/app/common-component/loader/loader.component';
 import { AreaCode } from 'src/app/store/models/area-code.model';
 import { NzDrawerService } from 'ng-zorro-antd/drawer';
 import { NzButtonModule } from 'ng-zorro-antd/button';
+import { CommonService } from '../../../service/common/common.service';
 
 @Component({
 	standalone: true,
@@ -23,9 +24,11 @@ import { NzButtonModule } from 'ng-zorro-antd/button';
 export class AreaCodeComponent {
 	unsubscribe$: any = new Subject<void>();
 	loader$: Observable<boolean> = of(true);
+	isMobile: boolean = false;
 	constructor(
 		private areaCodeStore: AreaCodeStoreService,
-		private drawerService: NzDrawerService
+		private drawerService: NzDrawerService,
+		private commonService: CommonService
 	) {}
 
 	ngOnInit() {
@@ -35,6 +38,11 @@ export class AreaCodeComponent {
 	intitalize() {
 		this.getLoader();
 		this.isAreaCodeLoaded();
+		this.getScreenSize();
+	}
+
+	async getScreenSize() {
+		this.isMobile = await lastValueFrom(this.commonService.isItAMobile$());
 	}
 
 	getLoader() {
@@ -65,10 +73,10 @@ export class AreaCodeComponent {
 			'src/app/modules/secure/area-code/new-area-code/new-area-code.component'
 		);
 		this.drawerService.create({
-			nzTitle: 'New Area Code',
+			nzTitle: `${area ? 'Update': 'New'} Area Code`,
 			nzClosable: true,
 			nzMaskClosable: false,
-			nzWidth: '50%',
+			nzWidth: this.isMobile ? '100%' : '50%',
 			nzWrapClassName: 'full-drawer',
 			nzContent: NewAreaCodeComponent,
 			nzData: { area },
--- src/app/service/common/common.service.ts
+++ src/app/service/common/common.service.ts
@@ -3,128 +3,144 @@ import { LoaderModalComponent } from '../../common-component/loader-modal/loader
 import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
 import { ToastrService } from 'ngx-toastr';
 import { NzNotificationPlacement, NzNotificationService } from 'ng-zorro-antd/notification';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { first, map, Observable, take } from 'rxjs';
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class CommonService {
-  private loaderModal: NzModalRef<LoaderModalComponent> | null = null;
-  constructor(
-    private modalService: NzModalService,
-    private toastr: ToastrService,
-    private notification: NzNotificationService
-  ) {}
+	private loaderModal: NzModalRef<LoaderModalComponent> | null = null;
+	constructor(
+		private modalService: NzModalService,
+		private toastr: ToastrService,
+		private notification: NzNotificationService,
+		private breakPointObserver: BreakpointObserver
+	) {}
 
-  presentLoading(loading_text: string = 'Please wait...') {
-    if (this.loaderModal) return;
-    this.loaderModal = this.modalService.create({
-      nzContent: LoaderModalComponent,
-      nzClassName: 'loader-modal',
-      nzWidth: null as unknown as number,
-      nzMaskClosable: true,
-      nzFooter: null,
-      nzCentered: true,
-      nzCancelText: null,
-      nzClosable: false,
-      // nzData:loading_text    // for v15.1.0
-    });
-    // for v15.1.0 no need for this
-    const comp: any = this.loaderModal.getContentComponent();
-    comp.text = loading_text;
-  }
+	presentLoading(loading_text: string = 'Please wait...') {
+		if (this.loaderModal) return;
+		this.loaderModal = this.modalService.create({
+			nzContent: LoaderModalComponent,
+			nzClassName: 'loader-modal',
+			nzWidth: null as unknown as number,
+			nzMaskClosable: true,
+			nzFooter: null,
+			nzCentered: true,
+			nzCancelText: null,
+			nzClosable: false,
+			// nzData:loading_text    // for v15.1.0
+		});
+		// for v15.1.0 no need for this
+		const comp: any = this.loaderModal.getContentComponent();
+		comp.text = loading_text;
+	}
 
-  dismissLoading() {
-    if (this.loaderModal) {
-      this.loaderModal.destroy();
-      this.loaderModal = null;
-    }
-  }
+	dismissLoading() {
+		if (this.loaderModal) {
+			this.loaderModal.destroy();
+			this.loaderModal = null;
+		}
+	}
 
-  showConfirmModal(title: string): Promise<boolean> {
-    return new Promise((resolve) => {
-      this.modalService.confirm({
-        nzTitle: title,
-        nzOkText: 'Yes',
-        nzOkType: 'primary',
-        nzOkDanger: true,
-        nzCancelText: 'No',
-        nzClosable: false,
-        nzCentered: true,
-        nzOnOk: () => resolve(true),
-        nzOnCancel: () => resolve(false),
-      });
-    });
-  }
+	showConfirmModal(title: string): Promise<boolean> {
+		return new Promise((resolve) => {
+			this.modalService.confirm({
+				nzTitle: title,
+				nzOkText: 'Yes',
+				nzOkType: 'primary',
+				nzOkDanger: true,
+				nzCancelText: 'No',
+				nzClosable: false,
+				nzCentered: true,
+				nzOnOk: () => resolve(true),
+				nzOnCancel: () => resolve(false),
+			});
+		});
+	}
 
-  showInfoModal(title: string, message?: string){
-    this.modalService.info({
-      nzTitle: title,
-      nzContent: message || '',
-      // nzOnOk: () => console.log('Info OK')
-    });
+	showInfoModal(title: string, message?: string) {
+		this.modalService.info({
+			nzTitle: title,
+			nzContent: message || '',
+			// nzOnOk: () => console.log('Info OK')
+		});
+	}
 
-  }
+	private showToast(
+		type: string,
+		msg: string,
+		duration: number,
+		placement: NzNotificationPlacement = 'topRight'
+	): void {
+		this.notification.create(type, '', msg, {
+			nzDuration: duration,
+			nzPlacement: placement,
+		});
+	}
 
-  private showToast(
-    type: string,
-    msg: string,
-    duration: number,
-    placement: NzNotificationPlacement = 'topRight'
-  ): void {
-    this.notification.create(type, '', msg, {
-      nzDuration: duration,
-      nzPlacement: placement,
-    });
-  }
+	showSuccessToast(message: string = 'Successful') {
+		this.showToast('success', message, 3000);
+	}
 
-  showSuccessToast(message: string = 'Successful') {
-    this.showToast('success', message, 3000);
-  }
+	showErrorToast(message: string = 'Something went wrong') {
+		this.showToast('error', message, 3000);
+	}
 
-  showErrorToast(message: string = 'Something went wrong') {
-    this.showToast('error', message, 3000);
-  }
+	showSuccessModal(
+		message: string = 'Successful',
+		title: string = 'Success'
+	) {
+		this.modalService.success({
+			nzTitle: title,
+			nzContent: message,
+			nzClosable: false,
+			nzCentered: true,
+		});
+	}
+	showErrorModal(
+		message: string = 'Something went wrong',
+		title: string = 'Error'
+	) {
+		this.modalService.error({
+			nzTitle: title,
+			nzContent: message,
+			nzClosable: false,
+			nzCentered: true,
+		});
+	}
+	showWarningModal(
+		message: string = 'Please follow the instructions',
+		title: string = 'Warning'
+	) {
+		this.modalService.warning({
+			nzTitle: title,
+			nzContent: message,
+			nzClosable: false,
+			nzCentered: true,
+		});
+	}
 
-  showSuccessModal(
-    message: string = 'Successful',
-    title: string = 'Success',
-  ) {
-    this.modalService.success({
-      nzTitle: title,
-      nzContent: message,
-      nzClosable: false,
-      nzCentered: true,
-    });
-  }
-  showErrorModal(
-    message: string = 'Something went wrong',
-    title: string = 'Error',
-  ) {
-    this.modalService.error({
-      nzTitle: title,
-      nzContent: message,
-      nzClosable: false,
-      nzCentered: true,
-    });
-  }
-  showWarningModal(
-    message: string = 'Please follow the instructions',
-    title: string = 'Warning',
-  ) {
-    this.modalService.warning({
-      nzTitle: title,
-      nzContent: message,
-      nzClosable: false,
-      nzCentered: true,
-    });
-  }
+	decodeJWT() {
+		const token = sessionStorage.getItem('accessToken');
+		if (token) {
+			const base64Url = token.split('.')[1]; // Get the payload part
+			const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Replace URL-safe characters
+			const decodedPayload = JSON.parse(window.atob(base64)); // Decode and parse JSON
+			return decodedPayload;
+		}
+	}
 
-  decodeJWT() {
-    const token = sessionStorage.getItem('accessToken');
-    if (token) {
-      const base64Url = token.split('.')[1]; // Get the payload part
-      const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/'); // Replace URL-safe characters
-      const decodedPayload = JSON.parse(window.atob(base64)); // Decode and parse JSON
-      return decodedPayload;
-    }
-  }
+	isItAMobile$(): Observable<boolean> {
+		return this.breakPointObserver
+			.observe([
+				Breakpoints.HandsetPortrait,
+				Breakpoints.HandsetLandscape,
+			])
+			.pipe(
+				// Use 'first()' to get the initial match and complete the observable
+				first(),
+				// Use map to transform the ObserverResponse into a simple boolean
+				map((observer) => observer.matches)
+			);
+	}
 }
